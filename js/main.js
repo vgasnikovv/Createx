d32d981c781a5bb3e0d156ff7cf67b0a6513faa1
vgasnikovv <166342579+vgasnikovv@users.noreply.github.com>
2024-04-10 21:56:45 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var mixer = mixitup('.directions__list');
+
+  $('.directions__filter-btn').on('click', function () {
+    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+    $(this).addClass('directions__filter-btn--active')
+  })
